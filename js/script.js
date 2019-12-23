68fd1fff936994a1f76b997f5254b62687363709
Kunderenko <39592711+Kunderenko@users.noreply.github.com>
2019-12-23 15:57:07 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
-
 $('.lang__text').on('click', function(e) {
 	e.preventDefault;
 	$(this).toggleClass('active');
 	$('.lang-switch').toggleClass('active');
-})
+});
 
 $('.header_menu').on('click', function(e) {
 	e.preventDefault;
 	$(this).toggleClass('active');
 	$('.menu').toggleClass('active');
-	$('.menu_btn').toggleClass('active');
+	// $('.menu_btm').toggleClass('active');
 	$('.menu_text').toggleClass('active');
-})
+});
+
+$('.menu-btm').on('click', function(){
+	$(this).toggleClass('menu-btm_active');
+});
 
 $(function(){
 	$("input[name=phone]").mask("+38 (099) 999 99 99");
