6e44866ab67ba1a6c88e9aaa89ee49d843806979
Kunderenko <39592711+Kunderenko@users.noreply.github.com>
2020-01-16 17:15:06 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,55 +38,54 @@ $('.car_select-wrapper').on('click', function(e) {
 	$(this).toggleClass('active');
 });
 
-$('.examples__slider').slick({
-	dots: true,
-	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 3,
-	responsive: [
-		{
-		  breakpoint: 1110,
-		  settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2
-		  }
-		},
-		{
-		  breakpoint: 600,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		  }
-		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-	  ]
-});
-
-$('.reviews_slider').slick({
+$(".examples__slider").slick({
 	dots: true,
 	infinite: true,
 	slidesToShow: 3,
 	slidesToScroll: 1,
 	responsive: [
-		{
-		  breakpoint: 1110,
-		  settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2
-		  }
-		},
-		{
-		  breakpoint: 600,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		  }
+	  {
+		breakpoint: 1110,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2
 		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-	  ]
-
-});
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	]
+  });
+  
+  $(".reviews_slider").slick({
+	dots: true,
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	responsive: [
+	  {
+		breakpoint: 1110,
+		settings: {
+		  slidesToShow: 2,
+		  slidesToScroll: 2
+		}
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	]
+  });
